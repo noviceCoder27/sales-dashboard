2c6a3bdecc2cfd804913c0a5a89c7c0334b83d2c
mdGoswami <u19cse1001@cit.ac.in>
2024-04-01 13:03:09 +0530

    add navigate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {BrowserRouter as Router,Routes,Route, Navigate} from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Auth from './pages/Auth'
 import ProtectedRoutes from '../utils/ProtectedRoutes'
@@ -13,6 +13,7 @@ function App() {
         <Route path = "/dashboard" element = {<ProtectedRoutes><Dashboard /></ProtectedRoutes>}/>
         <Route path = "/auth" element = {<Auth />} />
         <Route path = "/list" element = {<ProtectedRoutes><ProductList /></ProtectedRoutes>} />
+        <Route path = "*" element = {<Navigate to = "/auth"/>} />
       </Routes>
     </Router>
   )
